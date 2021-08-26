d3c9e62bb48cfef46789d238073572d76e24d525
Dan Ware <scandal.uk@gmail.com>
2021-08-26 23:57:19 +0100

    Added unit tests for single/multi line output

--- test/QuickCompareTests/TableCompareTests.cs
+++ test/QuickCompareTests/TableCompareTests.cs
@@ -2,11 +2,13 @@
 {
     using FluentAssertions;
     using QuickCompareModel.DatabaseSchema;
+    using System.Collections.Generic;
     using Xunit;
 
     public class TableCompareTests
     {
         private const string TableName = "[dbo].[Table1]";
+        private const string TabIndent = "     ";
 
         [Fact]
         public void TableMissingFromDatabase1_IsReported()
@@ -24,7 +26,7 @@
             var diff = builder.Differences.TableDifferences[TableName];
             diff.ExistsInDatabase1.Should().BeFalse();
             diff.ExistsInDatabase2.Should().BeTrue();
-            diff.ToString().Should().Be("does not exist in database 1\r\n");
+            diff.ToString().Should().Contain("does not exist in database 1");
         }
 
         [Fact]
@@ -43,7 +45,7 @@
             var diff = builder.Differences.TableDifferences[TableName];
             diff.ExistsInDatabase1.Should().BeTrue();
             diff.ExistsInDatabase2.Should().BeFalse();
-            diff.ToString().Should().Be("does not exist in database 2\r\n");
+            diff.ToString().Should().Contain("does not exist in database 2");
         }
 
         [Fact]
@@ -62,6 +64,8 @@
 
             var diff = builder.Differences.TableDifferences[TableName];
             diff.ExistsInBothDatabases.Should().BeTrue();
+
+            diff.IsDifferent.Should().BeFalse();
             diff.ToString().Should().Be(string.Empty);
         }
 
@@ -92,6 +96,9 @@
             var diff = builder.Differences.TableDifferences[TableName];
             diff.ExistsInBothDatabases.Should().BeTrue();
             diff.ExtendedPropertyDifferences.Count.Should().Be(1);
+            diff.ExtendedPropertyDifferences.ContainsKey("Key1").Should().BeTrue();
+
+            diff.ExtendedPropertyDifferences["Key1"].IsDifferent.Should().BeTrue();
             diff.ExtendedPropertyDifferences["Key1"].ExistsInDatabase1.Should().BeFalse();
 
             builder.Differences.TableDifferences[TableName]
@@ -124,7 +131,11 @@
 
             var diff = builder.Differences.TableDifferences[TableName];
             diff.ExistsInBothDatabases.Should().BeTrue();
+
             diff.ExtendedPropertyDifferences.Count.Should().Be(1);
+            diff.ExtendedPropertyDifferences.ContainsKey("Key1").Should().BeTrue();
+
+            diff.ExtendedPropertyDifferences["Key1"].IsDifferent.Should().BeTrue();
             diff.ExtendedPropertyDifferences["Key1"].ExistsInDatabase2.Should().BeFalse();
 
             builder.Differences.TableDifferences[TableName]
@@ -168,6 +179,8 @@
             var diff = builder.Differences.TableDifferences[TableName];
             diff.ExistsInBothDatabases.Should().BeTrue();
             diff.ExtendedPropertyDifferences.Count.Should().Be(1);
+
+            diff.ExtendedPropertyDifferences.ContainsKey("Key1").Should().BeTrue();
             diff.ExtendedPropertyDifferences["Key1"].ExistsInBothDatabases.Should().BeTrue();
             diff.ExtendedPropertyDifferences["Key1"].IsDifferent.Should().BeTrue();
         }
@@ -200,6 +213,7 @@
 
             diff.HasPermissionDifferences.Should().BeTrue();
             diff.PermissionDifferences.Count.Should().Be(1);
+            diff.PermissionDifferences.ContainsKey("[INSERT] DENIED for user: [foobar]").Should().BeTrue();
 
             builder.Differences.TableDifferences[TableName]
                 .ToString().Should().Contain("Permission: [INSERT] DENIED for user: [foobar] does not exist in database 1");
@@ -233,6 +247,7 @@
 
             diff.HasPermissionDifferences.Should().BeTrue();
             diff.PermissionDifferences.Count.Should().Be(1);
+            diff.PermissionDifferences.ContainsKey("[INSERT] DENIED for user: [foobar]").Should().BeTrue();
 
             builder.Differences.TableDifferences[TableName]
                 .ToString().Should().Contain("Permission: [INSERT] DENIED for user: [foobar] does not exist in database 2");
@@ -275,11 +290,59 @@
 
             diff.HasPermissionDifferences.Should().BeFalse();
             diff.PermissionDifferences.Count.Should().Be(1);
+            diff.PermissionDifferences.ContainsKey("[INSERT] DENIED for user: [foobar]").Should().BeTrue();
 
             diff.PermissionDifferences["[INSERT] DENIED for user: [foobar]"]
                 .ExistsInBothDatabases.Should().BeTrue();
 
             builder.Differences.TableDifferences[TableName].IsDifferent.Should().BeFalse();
+        }
+
+        [Fact]
+        public void TableColumn_WithSingleDifference_IsSingleLine()
+        {
+            // Arrange
+            var builder = TestHelper.GetBasicBuilder();
+            builder.Database1.Tables.Add(TableName, new SqlTable { ColumnDetails = new List<SqlColumnDetail> { new SqlColumnDetail { ColumnName = "foobar" } } });
+            builder.Database1.Tables[TableName].ColumnDetails[0].OrdinalPosition = 1;
+            builder.Database2.Tables.Add(TableName, new SqlTable { ColumnDetails = new List<SqlColumnDetail> { new SqlColumnDetail { ColumnName = "foobar" } } });
+
+            // Act
+            builder.BuildDifferences();
+
+            // Assert
+            builder.Differences.TableDifferences.Should().ContainKey(TableName);
+
+            var diff = builder.Differences.TableDifferences[TableName];
+            diff.ExistsInBothDatabases.Should().BeTrue();
+            diff.IsDifferent.Should().BeTrue();
+
+            diff.ToString().Should().Be($"\r\n{TabIndent}[foobar] has different ordinal position - is 1 in database 1 and is 0 in database 2\r\n");
+        }
+
+        [Fact]
+        public void TableColumn_WithMultipleDifferences_IsMultipleLines()
+        {
+            // Arrange
+            var builder = TestHelper.GetBasicBuilder();
+            builder.Database1.Tables.Add(TableName, new SqlTable { ColumnDetails = new List<SqlColumnDetail> { new SqlColumnDetail { ColumnName = "foobar" } } });
+            builder.Database1.Tables[TableName].ColumnDetails[0].OrdinalPosition = 1;
+            builder.Database1.Tables[TableName].ColumnDetails[0].NumericPrecision = 1;
+            builder.Database2.Tables.Add(TableName, new SqlTable { ColumnDetails = new List<SqlColumnDetail> { new SqlColumnDetail { ColumnName = "foobar" } } });
+
+            // Act
+            builder.BuildDifferences();
+
+            // Assert
+            builder.Differences.TableDifferences.Should().ContainKey(TableName);
+
+            var diff = builder.Differences.TableDifferences[TableName];
+            diff.ExistsInBothDatabases.Should().BeTrue();
+            diff.IsDifferent.Should().BeTrue();
+
+            diff.ToString().Should().StartWith($"\r\n{TabIndent}[foobar]");
+            diff.ToString().Should().Contain(" - has different ordinal position - is 1 in database 1 and is 0 in database 2\r\n");
+            diff.ToString().Should().Contain(" - has different numeric precision - is 1 in database 1 and is NULL in database 2\r\n");
         }
     }
 }
