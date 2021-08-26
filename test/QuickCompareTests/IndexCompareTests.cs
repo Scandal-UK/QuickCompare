745eafcecb75b87979a90ed91ecb1558daa0b726
Dan Ware <scandal.uk@gmail.com>
2021-08-26 20:19:30 +0100

    Added some tests for index types

--- test/QuickCompareTests/IndexCompareTests.cs
+++ test/QuickCompareTests/IndexCompareTests.cs
@@ -7,7 +7,7 @@
     public class IndexCompareTests
     {
         [Fact]
-        public void IndexMissingFromDatabase1_IsReported()
+        public void Index_MissingFromDatabase1_IsReported()
         {
             // Arrange
             var builder = TestHelper.GetBasicBuilder();
@@ -28,11 +28,12 @@
             var diff = builder.Differences.TableDifferences[tableName].IndexDifferences[indexName];
             diff.ExistsInDatabase1.Should().BeFalse();
             diff.ExistsInDatabase2.Should().BeTrue();
-            diff.ToString().Should().Be("does not exist in database 1\r\n");
+            builder.Differences.TableDifferences[tableName]
+                .ToString().Should().Contain($"Index: {indexName} does not exist in database 1");
         }
 
         [Fact]
-        public void IndexMissingFromDatabase2_IsReported()
+        public void Index_MissingFromDatabase2_IsReported()
         {
             // Arrange
             var builder = TestHelper.GetBasicBuilder();
@@ -53,11 +54,12 @@
             var diff = builder.Differences.TableDifferences[tableName].IndexDifferences[indexName];
             diff.ExistsInDatabase1.Should().BeTrue();
             diff.ExistsInDatabase2.Should().BeFalse();
-            diff.ToString().Should().Be("does not exist in database 2\r\n");
+            builder.Differences.TableDifferences[tableName]
+                .ToString().Should().Contain($"Index: {indexName} does not exist in database 2");
         }
 
         [Fact]
-        public void IndexInBothDatabases_AreNotReported()
+        public void Index_InBothDatabases_IsNotReported()
         {
             // Arrange
             var builder = TestHelper.GetBasicBuilder();
@@ -79,6 +81,98 @@
             var diff = builder.Differences.TableDifferences[tableName].IndexDifferences[indexName];
             diff.ExistsInBothDatabases.Should().BeTrue();
             diff.IsDifferent.Should().BeFalse();
+        }
+
+        [Fact]
+        public void IndexType_PrimaryKey_MissingFromDatabase1_IsReported()
+        {
+            // Arrange
+            var builder = TestHelper.GetBasicBuilder();
+
+            var tableName = "Table1";
+            var indexName = "Index1";
+            builder.Database1.Tables.Add(tableName, new SqlTable());
+            builder.Database2.Tables.Add(tableName, new SqlTable());
+            builder.Database2.Tables[tableName].Indexes.Add(new SqlIndex { IndexName = indexName, IsPrimaryKey = true });
+
+            // Act
+            builder.BuildDifferences();
+
+            // Assert
+            builder.Differences.TableDifferences[tableName]
+                .IndexDifferences.Should().ContainKey(indexName);
+
+            var diff = builder.Differences.TableDifferences[tableName];
+            diff.ToString().Should().Contain($"Primary key: {indexName} does not exist in database 1");
+        }
+
+        [Fact]
+        public void IndexType_PrimaryKey_MissingFromDatabase2_IsReported()
+        {
+            // Arrange
+            var builder = TestHelper.GetBasicBuilder();
+
+            var tableName = "Table1";
+            var indexName = "Index1";
+            builder.Database1.Tables.Add(tableName, new SqlTable());
+            builder.Database1.Tables[tableName].Indexes.Add(new SqlIndex { IndexName = indexName, IsPrimaryKey = true });
+            builder.Database2.Tables.Add(tableName, new SqlTable());
+
+            // Act
+            builder.BuildDifferences();
+
+            // Assert
+            builder.Differences.TableDifferences[tableName]
+                .IndexDifferences.Should().ContainKey(indexName);
+
+            var diff = builder.Differences.TableDifferences[tableName];
+            diff.ToString().Should().Contain($"Primary key: {indexName} does not exist in database 2");
+        }
+
+        [Fact]
+        public void IndexType_UniqueKey_MissingFromDatabase1_IsReported()
+        {
+            // Arrange
+            var builder = TestHelper.GetBasicBuilder();
+
+            var tableName = "Table1";
+            var indexName = "Index1";
+            builder.Database1.Tables.Add(tableName, new SqlTable());
+            builder.Database2.Tables.Add(tableName, new SqlTable());
+            builder.Database2.Tables[tableName].Indexes.Add(new SqlIndex { IndexName = indexName, IsUniqueKey = true });
+
+            // Act
+            builder.BuildDifferences();
+
+            // Assert
+            builder.Differences.TableDifferences[tableName]
+                .IndexDifferences.Should().ContainKey(indexName);
+
+            var diff = builder.Differences.TableDifferences[tableName];
+            diff.ToString().Should().Contain($"Unique key: {indexName} does not exist in database 1");
+        }
+
+        [Fact]
+        public void IndexType_UniqueKey_MissingFromDatabase2_IsReported()
+        {
+            // Arrange
+            var builder = TestHelper.GetBasicBuilder();
+
+            var tableName = "Table1";
+            var indexName = "Index1";
+            builder.Database1.Tables.Add(tableName, new SqlTable());
+            builder.Database1.Tables[tableName].Indexes.Add(new SqlIndex { IndexName = indexName, IsUniqueKey = true });
+            builder.Database2.Tables.Add(tableName, new SqlTable());
+
+            // Act
+            builder.BuildDifferences();
+
+            // Assert
+            builder.Differences.TableDifferences[tableName]
+                .IndexDifferences.Should().ContainKey(indexName);
+
+            var diff = builder.Differences.TableDifferences[tableName];
+            diff.ToString().Should().Contain($"Unique key: {indexName} does not exist in database 2");
         }
     }
 }
